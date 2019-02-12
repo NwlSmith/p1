f47fe060b5fff6c9c072dc2c8da7fe71b89c0045
Nate Smith <me@nwlsmith.com>
2019-02-12 17:53:06 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>p1</title>
+        <meta charset="utf-8" />
+    </head>
+    <h1>Nate Smith</h1>
+    <img src="cat.jpg" alt="me">
+    <p>Hi everyone!<br>
+        My name is Nate and I am from Jamaica Plain, MA. I work at a place called Einstein's Workshop, a STEM education center for middle school aged students, where I teach a class on 3D printing. I am currently on a gap year as I transfer from one university to another, and in the meantime, I am taking courses to fulfill requirements and also courses that I think will be useful in the future. I am planning on entering a career in game design with a focus on programming, and I am currently developing a curriculum for a new course at my job on making games using Unity.</p>
+
+    <body>
+
+    </body>
+
+</html>

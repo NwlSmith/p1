a3d13294787ea005f92ae2edd7c846e439edeff0
Nate Smith <me@nwlsmith.com>
2019-02-12 18:20:40 -0500

    added functionality and style to index.php

--- index.php
+++ index.php
@@ -3,11 +3,35 @@
     <head>
         <title>p1</title>
         <meta charset="utf-8" />
+        <link href="https://fonts.googleapis.com/css?family=Raleway:200,200i" rel="stylesheet">
+        <style>
+            body {
+                background-color: rgb(5, 5, 5);
+
+                text-align: center;
+                font-family: Raleway, Impact, "Trebuchet MS", "Courier New";
+                color: rgb(240, 240, 240);
+                width: 60%;
+                margin-left: 20%;
+            }
+            
+            #quote {
+                border: 5px solid rgb(240, 240, 240);
+                font-size: 1.2em;
+            }
+            
+        </style>
+
+        <?php include("random.php");?>
     </head>
     <h1>Nate Smith</h1>
     <img src="cat.jpg" alt="me">
     <p>Hi everyone!<br>
         My name is Nate and I am from Jamaica Plain, MA. I work at a place called Einstein's Workshop, a STEM education center for middle school aged students, where I teach a class on 3D printing. I am currently on a gap year as I transfer from one university to another, and in the meantime, I am taking courses to fulfill requirements and also courses that I think will be useful in the future. I am planning on entering a career in game design with a focus on programming, and I am currently developing a curriculum for a new course at my job on making games using Unity.</p>
+
+    <p id="quote">
+        <?php echo $quote; ?>
+    </p>
 
     <body>
 
---
+++ random.php
@@ -0,0 +1,4 @@
+<?php 
+$quotes = ["meow", "meeeyoowwww", "hiss"];
+$quote = $quotes[rand(0, 2)];
+?>
